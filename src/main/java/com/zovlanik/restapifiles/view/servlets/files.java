738ecf07034a1d037876baabaa2f1783d34b03ce
Zovlanik <Dkflbvbh162>
2021-02-15 19:02:55 +0300

    немножко переделал класс file

--- src/main/java/com/zovlanik/restapifiles/view/servlets/files.java
+++ src/main/java/com/zovlanik/restapifiles/view/servlets/files.java
@@ -9,6 +9,7 @@ import com.zovlanik.restapifiles.repository.FileRepository;
 import com.zovlanik.restapifiles.repository.hibernate.HibernateFileRepositoryImpl;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,6 +23,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@MultipartConfig
 public class files extends HttpServlet {
     private final FileRepository fileRepository = new HibernateFileRepositoryImpl();
 
@@ -50,44 +52,27 @@ public class files extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("multipart/mixed");
 
+        File file = new File();
+        String fileName = extractFileName(request.getPart("filename"));
 
-//        try {
-//
-//            File newFile = new Gson().fromJson(request.getReader(), File.class);
-//            //String test = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-//            response.getWriter().println();
-//
-////            newFile.setFilename(request.getParameter("filename"));
-////            newFile.setUser_id(Integer.parseInt(request.getParameter("user_id")));
-//            if(newFile.getCreationDate() == null) {
-//                newFile.setCreationDate(new Date(System.currentTimeMillis()));
-//            }
-//            if(newFile.getStatus() == null){
-//                newFile.setStatus(FileStatus.ACTIVE);
-//            }
-//
-//            fileRepository.create(newFile);
-//            response.getWriter().println("File with filename = " + newFile.getFilename() + " was successfully created.");
-//
-//
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-        try{
-            BufferedReader st = request.getReader();
-            String filename = getFilenameFromBufferedReader(st);
-            response.getWriter().println(filename);
-        }catch (Exception exception){}
+        file.setFilename(fileName);
+        file.setCreationDate(new Date());
+        file.setStatus(FileStatus.ACTIVE);
+
+        fileRepository.create(file);
+        response.getWriter().println(new Gson().toJson(file, File.class));
+
     }
-    private String getFilenameFromBufferedReader(BufferedReader bufferedReader) throws IOException {
-        String result = "";
-        String line;
-        while((line = bufferedReader.readLine()) != null){
-            if(line.contains("filename")){
-                result = line.substring(line.lastIndexOf("filename")+9);
+
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
             }
         }
-        return result;
+        return "";
     }
 
     public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
